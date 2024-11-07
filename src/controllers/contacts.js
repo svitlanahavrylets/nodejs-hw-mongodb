f990dd8f638815e9433c40cc2ac6af32889c705b
svitlana_havrylets <svitlana.havrylets@gmail.com>
2024-11-07 23:31:29 +0100

    add err 404

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -1,6 +1,5 @@
 import createHttpError from 'http-errors';
 import * as contactServices from '../services/contacts.js';
-import mongoose from 'mongoose';
 
 export const getContactsController = async (req, res) => {
   const data = await contactServices.getContacts();
@@ -12,17 +11,13 @@ export const getContactsController = async (req, res) => {
   });
 };
 
-export const getContactsByIdController = async (req, res, next) => {
+export const getContactsByIdController = async (req, res) => {
   const { contactId } = req.params;
 
-  if (!mongoose.Types.ObjectId.isValid(contactId)) {
-    throw createHttpError(404, 'Contact not found');
-  }
   const data = await contactServices.getContactById(contactId);
 
   if (!data) {
-    next(createHttpError(404, 'Contact not found'));
-    return;
+    throw createHttpError(404, 'Contact not found');
   }
 
   res.json({
