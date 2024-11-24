dee5e6ba3437e3ffb3dbfa032eb43d492c2f9727
svitlana_havrylets <svitlana.havrylets@gmail.com>
2024-11-24 23:52:16 +0100

    login

---
+++ src/routers/auth.js
@@ -0,0 +1,23 @@
+import { Router } from 'express';
+import { loginUserSchema, registerUserSchema } from '../validation/auth.js';
+import {
+  loginUserController,
+  registerUserController,
+} from '../controllers/auth.js';
+import ctrlWrapper from '../utils/ctrlWrapper.js';
+import validateBody from '../middlewares/validateBody.js';
+
+const router = Router();
+
+router.post(
+  '/register',
+  validateBody(registerUserSchema),
+  ctrlWrapper(registerUserController),
+);
+
+router.post(
+  '/login',
+  validateBody(loginUserSchema),
+  ctrlWrapper(loginUserController),
+);
+export default router;
--- src/server.js
+++ src/server.js
@@ -2,9 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import pino from 'pino-http';
 import { env } from './utils/env.js';
-import contactsRouter from './routers/contacts.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
+import contactsRouter from './routers/contacts.js';
+import cookieParser from 'cookie-parser';
 
 const PORT = Number(env('PORT', 3030));
 
@@ -14,6 +15,8 @@ export const setupServer = () => {
   app.use(express.json());
 
   app.use(cors());
+
+  app.use(cookieParser());
 
   app.use(
     pino({
